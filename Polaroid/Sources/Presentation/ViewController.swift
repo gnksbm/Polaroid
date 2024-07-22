81143a8cf88b661f385e26c2ad8475e3c937a91e
Geonseob Kim <gnksbm@gmail.com>
2024-07-22 14:39:22 +0900

    Initial Commit

---
+++ Polaroid/Sources/Presentation/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Polaroid
+//
+//  Created by gnksbm on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
