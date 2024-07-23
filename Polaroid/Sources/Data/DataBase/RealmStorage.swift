c877e9197ac3e4173abfab90ff735f158e949871
Geonseob Kim <gnksbm@gmail.com>
2024-07-23 16:33:18 +0900

    [Feat] RealmStorage 구현

---
+++ Polaroid/Sources/Data/DataBase/RealmStorage.swift
@@ -0,0 +1,47 @@
+//
+//  RealmStorage.swift
+//  Polaroid
+//
+//  Created by gnksbm on 7/23/24.
+//
+
+import Foundation
+
+import RealmSwift
+
+final class RealmStorage {
+    private let realm = try! Realm()
+    
+    private init() { }
+    
+    func create(_ object: Object) throws {
+        try realm.write {
+            realm.add(object)
+        }
+    }
+    
+    func read<T: Object>(
+        _ type: T.Type
+    ) -> Results<T> {
+        realm.objects(type)
+    }
+    
+    func update<T: Object>(
+        _ object: T,
+        _ block: (T) -> Void
+    ) throws {
+        try realm.write {
+            block(object)
+        }
+    }
+    
+    func delete(_ object: Object) throws {
+        try realm.write {
+            realm.delete(object)
+        }
+    }
+}
+
+extension RealmStorage {
+    static let shared = RealmStorage()
+}
